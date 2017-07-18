bb0ec2e25ca708c626288cd6ab947ee0de5313ea
Oliver Wegner <void1976@gmail.com>
2017-07-18 15:10:39 +0200

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ow-app/lein-template "0.1.0-SNAPSHOT"
+(defproject ow-app/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
